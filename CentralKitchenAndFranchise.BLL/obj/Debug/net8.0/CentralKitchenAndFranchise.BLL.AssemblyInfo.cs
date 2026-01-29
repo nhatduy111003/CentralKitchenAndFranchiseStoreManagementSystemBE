f7a5c8c1f9288cf7f54695ae82e1e91f6175ff85
nhatduy111003 <phamnhatduy08@gmail.com>
2026-01-30 01:46:19 +0700

    Fix file vs and bin

--- CentralKitchenAndFranchise.BLL/obj/Debug/net8.0/CentralKitchenAndFranchise.BLL.AssemblyInfo.cs
+++ CentralKitchenAndFranchise.BLL/obj/Debug/net8.0/CentralKitchenAndFranchise.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentralKitchenAndFranchise.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c013e826936afc3cc48d06a8498a3a5241b1b205")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9b7848e180c488421c4e1a8cc5ec623a49d5f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentralKitchenAndFranchise.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentralKitchenAndFranchise.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
