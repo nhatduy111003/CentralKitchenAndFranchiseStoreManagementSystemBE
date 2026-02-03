06add17db3f8f27535356d574d20c677b54fda09
nhatduy111003 <phamnhatduy08@gmail.com>
2026-02-03 16:13:57 +0700

    API assign user to Franchise and Store

---
+++ CentralKitchenAndFranchise.API/Controllers/UserFranchiseController.cs
@@ -0,0 +1,42 @@
+﻿using CentralKitchenAndFranchise.BLL.Services.Interfaces;
+using CentralKitchenAndFranchise.DTO.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CentralKitchenAndFranchise.API.Controllers
+{
+    [ApiController]
+    [Route("admin/user-franchises")]
+    public class UserFranchiseController : ControllerBase
+    {
+        private readonly IUserFranchiseService _service;
+
+        public UserFranchiseController(IUserFranchiseService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Assign(AssignUserFranchiseDto dto)
+        {
+            await _service.AssignAsync(dto.UserId, dto.FranchiseId);
+            return Ok("Assigned");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Remove(int userId, int franchiseId)
+        {
+            await _service.RemoveAsync(userId, franchiseId);
+            return Ok("Removed");
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> Get(int userId)
+        {
+            var list = await _service.GetByUserAsync(userId);
+            return Ok(list);
+        }
+    }
+
+}
---
+++ CentralKitchenAndFranchise.DAL/Migrations/20260203073012_AddPermissions.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CentralKitchenAndFranchise.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPermissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "permissions",
+                columns: table => new
+                {
+                    PermissionId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Code = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    GroupName = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_permissions", x => x.PermissionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "role_permissions",
+                columns: table => new
+                {
+                    RoleId = table.Column<int>(type: "integer", nullable: false),
+                    PermissionId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_role_permissions", x => new { x.RoleId, x.PermissionId });
+                    table.ForeignKey(
+                        name: "FK_role_permissions_permissions_PermissionId",
+                        column: x => x.PermissionId,
+                        principalTable: "permissions",
+                        principalColumn: "PermissionId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_role_permissions_roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "roles",
+                        principalColumn: "RoleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_permissions_Code",
+                table: "permissions",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_role_permissions_PermissionId",
+                table: "role_permissions",
+                column: "PermissionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "role_permissions");
+
+            migrationBuilder.DropTable(
+                name: "permissions");
+        }
+    }
+}
