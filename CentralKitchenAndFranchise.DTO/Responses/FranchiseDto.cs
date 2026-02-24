6d9efcf008f7935b35b8d97174ddfea836c8426e
nhatduy111003 <phamnhatduy08@gmail.com>
2026-02-24 16:55:30 +0700

    Fix API Franchise

--- CentralKitchenAndFranchise.DTO/Responses/FranchiseDto.cs
+++ CentralKitchenAndFranchise.DTO/Responses/FranchiseDto.cs
@@ -14,6 +14,9 @@ namespace CentralKitchenAndFranchise.DTO.Responses
         public string Status { get; set; } = null!;
         public string? Address { get; set; }
         public string? Location { get; set; }
+        public double? Latitude { get; set; }
+
+        public double? Longitude { get; set; }
     }
 
 }
